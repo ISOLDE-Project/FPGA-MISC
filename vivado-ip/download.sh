3a37181a56797b1ca0e0d7c2be631a0d50909648
Daniel Grosu <daniel.grosu@continental-corporation.com>
2025-05-26 18:42:36 +0300

    Updated download and unzip scripts

--- vivado-ip/download.sh
+++ vivado-ip/download.sh
@@ -1,5 +1,7 @@
-wget  https://github.com/ISOLDE-Project/FPGA-MISC/releases/download/rc0.2/aiml_stub-vcu118.zip
-wget  https://github.com/ISOLDE-Project/FPGA-MISC/releases/download/rc0.2/isolde_resizer-vcu118.zip
+#wget  https://github.com/ISOLDE-Project/FPGA-MISC/releases/download/rc0.2/aiml_stub-vcu118.zip
+#wget  https://github.com/ISOLDE-Project/FPGA-MISC/releases/download/rc0.2/isolde_resizer-vcu118.zip
 #wget  https://github.com/ISOLDE-Project/FPGA-MISC/releases/download/rc0.0/sensorSupervisor-zcu104.zip
 #wget  https://github.com/ISOLDE-Project/FPGA-MISC/releases/download/rc0.0/LEDSupervisor-zcu104.zip
-
+wget https://github.com/ISOLDE-Project/FPGA-MISC/releases/download/rc0.3/img2axis-zcu102.zip
+wget https://github.com/ISOLDE-Project/FPGA-MISC/releases/download/rc0.3/isolde_resizer-zcu102.zip
+wget https://github.com/ISOLDE-Project/FPGA-MISC/releases/download/rc0.3/sensorSupervisor-zcu102.zip
--- vivado-ip/unzip.sh
+++ vivado-ip/unzip.sh
@@ -1,4 +1,6 @@
- unzip aiml_stub-zcu104.zip         -d aiml_stub-vcu118
- unzip aiml_stub-zcu104.zip         -d isolde_resizer-vcu118
+ #unzip aiml_stub-vcu118.zip         -d aiml_stub-vcu118
  #unzip sensorSupervisor-zcu104.zip  -d sensorSupervisor-zcu104
  #unzip LEDSupervisor-zcu104.zip     -d LEDSupervisor-zcu104
+unzip img2axis-zcu102.zip         -d img2axis-zcu102
+unzip isolde_resizer-zcu102.zip   -d isolde_resizer-zcu102
+unzip sensorSupervisor-zcu102.zip -d sensorSupervisor-zcu102
